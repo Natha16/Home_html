662dc364bcd8b684a5dd1623dfdc73c53c72fb04
Natha16 <161873686+Natha16@users.noreply.github.com>
2024-03-22 11:12:35 -0300

    Create code.js

---
+++ code.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const listaNomes= document.body.querySelector ('#listaNomes');
+    const inputNome= document.body.querySelector ('#inputnome');
+    const buttonAdicionar= document.body.querySelector ('#buttonAdicionar');
+    buttonAdicionar.addEventListener('click', () => {
+        const div= document.createElement ('div');
+        div.innerHTML = '<li>' + inputNome.value + '</li>';
+        listaNomes.appendChild(div.firstElementChild);
+    });
+});
